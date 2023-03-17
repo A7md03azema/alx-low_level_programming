fa814930662dbae0d18ed1f9bc8919f0020a2532
A7md03azema <ahmedabdelaziem696@gmail.com>
2023-03-18 00:07:05 +0200

    a-zOC

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+
+int main(void)
+{
+	for(int x = 'A'; x <= 'Z'; x++)
+	{
+	int lower_x = tolower(x);
+	putchar(lower_x);
+	}
+return(0);	
+}
