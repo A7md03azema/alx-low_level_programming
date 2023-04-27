eb3ca2a23bdc68b906dc3e30a730d1f171025641
A7md03azema <ahmedabdelaziem696@gmail.com>
2023-04-27 15:43:10 +0200

    print list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,20 +1,19 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stdlib.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
  * Description: singly linked list node structure
- * for Holberton project
+ *              for Holberton project
  */
 
 typedef struct list_s
 {
 	char *str;
-
 	unsigned int len;
-
 	struct list_s *next;
 } list_t;
 
@@ -24,4 +23,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif
+#endif /* LISTS_H */
